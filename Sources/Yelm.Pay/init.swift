7f65a18df7620e3ef0384785eb2059141de58dab
Michael Safir <admin@yelm.io>
2021-01-23 14:04:10 +0300

    setup fix

--- Sources/Yelm.Pay/init.swift
+++ Sources/Yelm.Pay/init.swift
@@ -20,7 +20,13 @@ open class Pay: ObservableObject, Identifiable {
     public var apple_pay : ApplePay = ApplePay()
     
     
-    public func start(platform : String, auth: String, d3ds : String, charge : String, payment_url : String, completionHandlerStart: @escaping (_ success:Bool) -> Void){
+    public func start(platform : String,
+                      auth: String = "cp_auth.php",
+                      d3ds : String = "cp_post3ds.php",
+                      charge : String = "cp_charge.php",
+                      payment_url : String = "https://api.yelm.io/payments/",
+                      completionHandlerStart: @escaping (_ success:Bool) -> Void){
+        
         self.settings.platform = platform
         self.settings.auth = auth
         self.settings.charge = charge
